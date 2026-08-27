e55975f853872355205af79accb09ef6f5a66ba5
Gabriel Alvis~ <gabrielnogueira6936@gmail.com>
2026-08-27 09:57:53 -0300

    primeiro teste da lista de animes já pronta

--- app.py
+++ app.py
@@ -1,9 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask (__name__)
+app = Flask(__name__)
+
+meus_animes = [
+    {
+        "nome": "Baki",
+        "total_eps": 26,
+        "eps_vistos": 7,
+        "finalizado": False
+    },
+    {
+        "nome": "Death Note",
+        "total_eps": 37,
+        "eps_vistos": 37,
+        "finalizado": True
+    },
+    {
+        "nome": "Giji Harem",
+        "total_eps": 12,
+        "eps_vistos": 3,
+        "finalizado": False
+    }
+]
 
 @app.route("/")
-def index ():
-    return "Opa"
+def index():
+    return render_template("listadeanimes.html", meus_animes=meus_animes)
+
 if __name__ == "__main__":
-    app.run(port=4567, debug=True)
+    app.run(port =4567, debug=True)
