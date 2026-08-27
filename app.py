009709b1b5ff5bb314578214c04705dd347b5a9c
Gabriel Alvis~ <gabrielnogueira6936@gmail.com>
2026-08-27 11:00:53 -0300

    primeiro teste com atualização de mudar o numero de eps

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request
 
 app = Flask(__name__)
-
+# Aqui criamos a nossa lista de animes (nossa base de dados de teste)
 meus_animes = [
     {
         "nome": "Baki",
@@ -25,7 +25,25 @@ meus_animes = [
 
 @app.route("/")
 def index():
+    # O render_template vai buscar um arquivo HTML e nós 'injetamos' a lista nele
     return render_template("listadeanimes.html", lista_animes=meus_animes)
+
+@app.route("/atualizar", methods=["POST"])
+def atualizar():
+    # Pega as informações que vieram do HTML
+    nome = request.form.get("nome_anime")
+    novos_eps = int(request.form.get("novos_eps"))
+
+    # Procura o anime na lista e atualiza o número
+    for anime in meus_animes:
+        if anime["nome"] == nome:
+            anime["eps_vistos"] = novos_eps
+            # Se o número de episódios vistos for igual ao total, finaliza automaticamente!
+            if novos_eps >= anime["total_eps"]:
+                anime["finalizado"] = True
+            break
+
+    return redirect("/")
 
 if __name__ == "__main__":
     app.run(port =4567, debug=True)
